6547c98786012277785ae1453b9d459739577184
ishio <ishio@itsj.jp>
2024-12-25 11:34:02 +0900

    markdown関連試し中

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -2,7 +2,12 @@ return{
     {
         "MeanderingProgrammer/markdown.nvim",
         main = "render-markdown",
-        opts = { enabled = true },
+        opts = {
+            enabled = true,
+            renderer = "html",
+            filetypes = { "markdown", "md", "txt" },
+            template = "default",
+        },
         dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
     }
 }
