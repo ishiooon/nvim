416eb235522bd07b146d8bfadd47b9baf93c4a45
ishio <ishio@itsj.jp>
2025-02-07 09:30:54 +0900

    タブインデント調整中

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,18 +10,19 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- タブとインデントの設定
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-local filetype_tabstop = {typescript=2, typescriptreact=2} -- filetype毎のインデント幅
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true -- タブをスペースに変換
+
+local filetype_tabstop = {php = 4} -- filetype毎のインデント幅
 local usrftcfg = vim.api.nvim_create_augroup("UserFileTypeConfig", { clear = true})
 vim.api.nvim_create_autocmd("FileType", {
   group = usrftcfg,
   callback = function (args)
     local ftts = filetype_tabstop[args.match]
     if ftts then
-      vim.bo.tabstop = ftts
-      vim.bo.shiftwidth = ftts
+      vim.opt.tabstop = ftts
+      vim.opt.shiftwidth = ftts
     end
   end
 })
